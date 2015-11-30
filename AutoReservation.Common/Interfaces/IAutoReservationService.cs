9b24048990ea5f26becd76eb524777e7bf6fc6b2
Pascal <pforster@hsr.ch>
2015-11-30 11:22:52 +0100

    implementet service interface

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,7 +1,38 @@
-﻿namespace AutoReservation.Common.Interfaces
+﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+
+namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
-        
+        IList<AutoDto> GetAutos();
+
+        AutoDto GetAuto(int id);
+
+        void AddAuto(AutoDto auto);
+
+        void DeleteAuto(AutoDto auto);
+
+        void UpdateAuto(AutoDto modified, AutoDto original);
+
+        IList<ReservationDto> GetReservations();
+
+        ReservationDto GetReservation(int id);
+
+        void AddReservation(ReservationDto reservation);
+
+        void DeleteReservation(ReservationDto reservation);
+
+        void UpdateReservation(ReservationDto modified, ReservationDto original);
+
+        IList<KundeDto> GetKunden();
+
+        KundeDto GetKunde(int id);
+
+        void AddKunde(KundeDto kunde);
+
+        void DeleteKunde(KundeDto kunde);
+
+        void UpdateKunde(KundeDto modified, KundeDto original);
     }
 }
